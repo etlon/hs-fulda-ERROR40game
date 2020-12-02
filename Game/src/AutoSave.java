e2274607d551bb4deab73048bf70c23af6d19379
Max Leibold <max.leibold@informatik.hs-fulda.de>
2020-12-02 13:19:54 +0100

    Add class AutoSave

---
+++ Game/src/AutoSave.java
@@ -0,0 +1,27 @@
+public class AutoSave extends Thread{
+
+    private String key = "score";
+    private String value = "";
+    private FileManager fm;
+    private AmountTotalClicksLabel aTCL;
+
+    public AutoSave(FileManager fm, AmountTotalClicksLabel aTCL){
+        this.fm = fm;
+        this.aTCL = aTCL;
+    }
+
+    public void run(){
+        try {
+
+            while (true) {
+                Thread.sleep(600000);  //10 minutes
+                value = aTCL.getCount();
+                fm.addValueByKey(key, value);
+                fm.save();
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
