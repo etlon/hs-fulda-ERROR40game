baa4f1b329bf01ae7fd292aeb37540a20522ccd9
Max Leibold <max.leibold@informatik.hs-fulda.de>
2020-12-16 09:24:59 +0100

    add Constructor of Grandma

--- src/buyables/Grandma.java
+++ src/buyables/Grandma.java
@@ -3,6 +3,9 @@ package buyables;
 public class Grandma extends ShopItem{
 
     public Grandma(){
-
+        this.name = "Grandma";
+        this.amount = 0;
+        this.basePrice = 500;
+        this.baseIncome = 5;
     }
 }
