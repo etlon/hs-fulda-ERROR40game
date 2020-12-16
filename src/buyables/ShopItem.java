ee36b7e45b57409777fe0a84f9407c9b66232e38
Nils Schiffhauer <nils.schiffhauer@informatik.hs-fulda.de>
2020-12-16 08:33:20 +0100

    Create abstract base Item for Shop

--- src/buyables/ShopItem.java
+++ src/buyables/ShopItem.java
@@ -1,13 +1,46 @@
 package buyables;
 
-public class ShopItem
+public abstract class ShopItem
 {
-
+    private double basePrice;
+    private int amount;
+    private double price;
+    private double baseIncome;
 
 
     public ShopItem()
     {
+        this.amount = 0;
 
+
+    }
+
+    public void setAmount(int amount)
+    {
+        this.amount = amount;
+    }
+
+    public void buy()
+    {
+        this.amount++;
+        this.calcPrice();
+
+    }
+
+
+
+    private void calcPrice()
+    {
+        this.price = basePrice + basePrice * amount * 0.2;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public double getIncome()
+    {
+        return  amount * baseIncome;
     }
 
 }
