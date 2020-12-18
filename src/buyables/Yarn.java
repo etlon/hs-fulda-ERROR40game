24829b445ccfc7600fed3edf93c9f08f288b4e0f
Cedric Nolte <feythfx@gmail.com>
2020-12-18 17:24:49 +0100

    add Yarn item

---
+++ src/buyables/Yarn.java
@@ -0,0 +1,14 @@
+package buyables;
+
+public class Yarn extends ShopItem {
+
+    public Yarn() {
+        this.name = "Yarn";
+        this.amount = 0;
+        this.basePrice = 100;
+        this.baseIncome = 1;
+    }
+
+
+
+}
