e12d9351e6f1e82b90e1e3d029ece81a462c6c07
Max Leibold <max.leibold@informatik.hs-fulda.de>
2020-12-19 14:45:08 +0100

    add methods to Shop

--- src/Shop.java
+++ src/Shop.java
@@ -1,13 +1,34 @@
+import buyables.ShopItem;
+
 public class Shop
 {
+    private ShopItem[] itemList;
 
     public Shop()
     {
-
+        this.fillItemList();
     }
 
     public double getIncome()
     {
-        return 1;
+        double income = 0;
+        for (ShopItem item : itemList) {
+            income += item.getIncome();
+        }
+        return income;
+    }
+
+    public void fillItemList(){
+        //create every buyable item once and put it into "itemList"
+    }
+
+    public void saveItems(){
+        for (ShopItem item : itemList) {
+            Main.fm.addValueByKey(item.getName(), "" + item.getAmount());
+        }
+    }
+
+    public int getAmountItems(){
+        return itemList.length;
     }
 }
