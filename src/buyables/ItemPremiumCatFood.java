31e45680bc81bfd99787e911cd67d15794f8b0a9
Max Leibold <max.leibold@informatik.hs-fulda.de>
2020-12-19 14:49:58 +0100

    add ItemPremiumCatFood

---
+++ src/buyables/ItemPremiumCatFood.java
@@ -0,0 +1,11 @@
+package buyables;
+
+public class ItemPremiumCatFood extends ShopItem{
+
+    public ItemPremiumCatFood() {
+        this.name = "Premium Cat Food";
+        this.amount = 0;
+        this.basePrice = 1000;
+        this.baseIncome = 10;
+    }
+}
