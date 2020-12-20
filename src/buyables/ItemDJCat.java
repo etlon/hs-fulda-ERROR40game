9395d2a6bca9f9a98c151f74ec2aa08435936132
Max Leibold <max.leibold@informatik.hs-fulda.de>
2020-12-20 15:53:14 +0100

    add DJCat

---
+++ src/buyables/ItemDJCat.java
@@ -0,0 +1,11 @@
+package buyables;
+
+public class ItemDJCat extends ShopItem{
+
+    public ItemDJCat() {
+        this.name = "DJ Cat";
+        this.amount = 0;
+        this.basePrice = 14000;
+        this.baseIncome = 50;
+    }
+}
