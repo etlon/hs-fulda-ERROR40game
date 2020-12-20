13f54a23a36ebaca20eb71d9704e4cacad855648
Cedric Nolte <feythfx@gmail.com>
2020-12-20 16:39:40 +0100

    fix counter

--- src/com/layout/ItemBuyButton.java
+++ src/com/layout/ItemBuyButton.java
@@ -19,7 +19,7 @@ public class ItemBuyButton extends JButton {
             BigDecimal dm = new BigDecimal(Main.amountTotalClicks.getCount());
             if(dm.compareTo(new BigDecimal("" + item.getPrice())) >= 0) {
                 item.buy();
-                Main.amountTotalClicks.setText(Main.amountTotalClicks.formatCounter(new BigDecimal(String.valueOf(item.getPrice()))));
+                Main.amountTotalClicks.increaseCounter(String.valueOf(0 - item.getPrice()));
                 System.out.println("gekauft: " + item.getName() + " " + item.getPrice());
             } else {
                 System.out.println("izz nicht");
