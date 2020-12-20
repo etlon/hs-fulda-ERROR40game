99f3473a0d0b7685162173a67534f2d0da09f727
Max Leibold <max.leibold@informatik.hs-fulda.de>
2020-12-20 15:45:56 +0100

    balance item grandma

--- src/buyables/ItemGrandma.java
+++ src/buyables/ItemGrandma.java
@@ -5,7 +5,7 @@ public class ItemGrandma extends ShopItem{
     public ItemGrandma(){
         this.name = "Grandma";
         this.amount = 0;
-        this.basePrice = 500;
-        this.baseIncome = 5;
+        this.basePrice = 600;
+        this.baseIncome = 4;
     }
 }
