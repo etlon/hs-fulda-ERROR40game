4d9e2796335a03485f45073460fc65672270edd3
Max Leibold <max.leibold@informatik.hs-fulda.de>
2020-12-20 16:11:06 +0100

    add itemBlanket

---
+++ src/buyables/ItemBlanket.java
@@ -0,0 +1,11 @@
+package buyables;
+
+public class ItemBlanket extends ShopItem{
+
+    public ItemBlanket() {
+        this.name = "Blanket";
+        this.amount = 0;
+        this.basePrice = 120000;
+        this.baseIncome = 240;
+    }
+}
