81561333918bb43a5ea8384282a2a777d729877f
Max Leibold <max.leibold@informatik.hs-fulda.de>
2020-12-28 15:10:20 +0100

    add indicator to PassiveIncomeLabel

--- src/com/layout/PassiveIncomeLabel.java
+++ src/com/layout/PassiveIncomeLabel.java
@@ -12,13 +12,13 @@ public class PassiveIncomeLabel extends JLabel {
     public PassiveIncomeLabel(int x, int y, int width, int height) {
 
         this.setBounds(x,y,width,height);
-        this.setBorder(new EmptyBorder(0,10,0,0));
+        this.setBorder(new EmptyBorder(0,5,0,0));
         this.updatePassiveIncome();
         this.setFont(new Font("Arial Black", Font.PLAIN, 18));
 
     }
 
     public void updatePassiveIncome(){
-        this.setText(Main.amountTotalClicks.formatCounter(new BigDecimal(String.valueOf(Main.shop.getIncome()))));
+        this.setText("<html>" + "per sec" +"<br />" + Main.amountTotalClicks.formatCounter(new BigDecimal(String.valueOf(Main.shop.getIncome()))) + "</html>");
     }
 }
