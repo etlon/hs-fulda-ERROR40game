748065c1f1f59ae0269c2571e5e254f093fd6e09
Cedric Nolte <feythfx@gmail.com>
2020-12-31 00:43:30 +0100

    reformat ItemBuyButton

--- src/com/layout/ItemBuyButton.java
+++ src/com/layout/ItemBuyButton.java
@@ -16,50 +16,46 @@ public class ItemBuyButton extends JButton {
 
     public ItemBuyButton(ItemBuyButton[] buttons, int index, ShopItem item) {
         this.item = item;
-        this.buttons=buttons;
-        this.index=index;
-        if(this.index!=0){
-            if(buttons[this.index-1].getItem().getAmount()==0) super.setEnabled(false);
+        this.buttons = buttons;
+        this.index = index;
+        if (this.index != 0) {
+            if (buttons[this.index - 1].getItem().getAmount() == 0) super.setEnabled(false);
             else this.runBuyableTest();
-        }
-        else this.runBuyableTest();
+        } else this.runBuyableTest();
 
         String price = ToolManager.formatCounter(BigDecimal.valueOf(item.getPrice()));
-        this.setText("<html>" + item.getName() +"<br />" + price + "<br/>" + item.getAmount() + "</html>");
-        this.setPreferredSize(new Dimension(120,100));
+        this.setText("<html>" + item.getName() + "<br />" + price + "<br/>" + item.getAmount() + "</html>");
+        this.setPreferredSize(new Dimension(120, 100));
         this.setHorizontalAlignment(SwingConstants.CENTER);
         this.addActionListener(e -> {
             BigDecimal dm = new BigDecimal(Main.amountTotalClicks.getCount());
             double itemPrice = item.getPrice();
-            if(dm.compareTo(new BigDecimal(String.valueOf(itemPrice))) >= 0) {
+            if (dm.compareTo(new BigDecimal(String.valueOf(itemPrice))) >= 0) {
                 Main.amountTotalClicks.increaseCounter(String.valueOf(0 - itemPrice));
                 item.buy();
                 String name = item.getName();
                 String s = ToolManager.formatCounter(new BigDecimal(String.valueOf(item.getPrice())));
 
-                this.setText("<html>" + item.getName() +"<br />" + s + "<br/>" + item.getAmount() + "</html>");
+                this.setText("<html>" + item.getName() + "<br />" + s + "<br/>" + item.getAmount() + "</html>");
                 //System.out.println("gekauft: " + item.getName() + " " + itemPrice + " " + item.getAmount());
                 Main.passiveIncomeLabel.updatePassiveIncome();
 
-                if(item.getAmount()>0&&index< buttons.length-1)
-                {
-                    buttons[index+1].setEnabled(true);
-                    buttons[index+1].runBuyableTest();
+                if (item.getAmount() > 0 && index < buttons.length - 1) {
+                    buttons[index + 1].setEnabled(true);
+                    buttons[index + 1].runBuyableTest();
                 }
             }
         });
     }
-    private ShopItem getItem()
-    {
+
+    private ShopItem getItem() {
         return item;
     }
 
-    private void runBuyableTest()
-    {
-        Thread t=new IsItemBuyableThread(item,this);
+    private void runBuyableTest() {
+        Thread t = new IsItemBuyableThread(item, this);
         t.start();
     }
-
 
 
 }
