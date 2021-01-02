7dc47cb33124c4cb3ecd7248dce1447c1ac6b8c8
Cedric Nolte <feythfx@gmail.com>
2021-01-02 14:59:29 +0100

    cleanup code

--- src/com/layout/BuyMenu.java
+++ src/com/layout/BuyMenu.java
@@ -13,10 +13,6 @@ public class BuyMenu extends JLabel {
         //this.setBorder(new EmptyBorder(0,10,0,0));
         this.setFont(new Font("Arial Black", Font.PLAIN, 30));
         this.setVerticalAlignment(JLabel.BOTTOM);
-        //this.setOpaque(true);
-        //this.setBackground(new Color(0xBD0000));
-
-
     }
 
     public void addPanels(ShopItem[] list) {
@@ -31,6 +27,4 @@ public class BuyMenu extends JLabel {
             this.add(panel);
         }
     }
-
-
 }
