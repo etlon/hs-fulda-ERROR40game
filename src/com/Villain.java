af0930ab3990e66c93387bb98b16d4c729710138
Cedric Nolte <feythfx@gmail.com>
2021-01-02 14:14:00 +0100

    fix villain spawning in middle button

--- src/com/Villain.java
+++ src/com/Villain.java
@@ -1,21 +1,17 @@
 package com;
 
 import javax.swing.*;
-import java.awt.*;
-import java.util.TimerTask;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.Timer;
 
 public class Villain extends JButton {
 
     private int x;
     private int y;
-    private int width;
-    private int height;
-    private long startTime;
-    private boolean isClicked;
+    private final int width;
+    private final int height;
+    private final long startTime;
+    private static boolean isClicked;
     private static boolean isSpawned;
-
 
     public Villain(int buttonWidth, int buttonHeight) {
 
@@ -29,7 +25,6 @@ public class Villain extends JButton {
             this.setEnabled(false);
             this.setVisible(false);
         });
-
     }
 
     private void spawnVillain() {
@@ -38,13 +33,14 @@ public class Villain extends JButton {
             try {
                 Thread.sleep(0);
 
-                while(true) {
-                    while(!isSpawned) {
+                while (true) {
+                    while (!isSpawned) {
 
                         int maxWidth = Main.mainClickerMiddleLayout.getWidth() - width;
                         int maxHeight = Main.mainClickerMiddleLayout.getHeight() - height;
-                        int randomX = ThreadLocalRandom.current().nextInt(0, maxWidth);
-                        int randomY = ThreadLocalRandom.current().nextInt(0, maxHeight);
+                        int randomX;
+                        int randomY;
+
 
                         int buttonXleft = Main.mainClickerMiddleButton.getX();
                         int buttonXright = Main.mainClickerMiddleButton.getX() + Main.mainClickerMiddleButton.getWidth();
@@ -56,18 +52,15 @@ public class Villain extends JButton {
                         int allowedYtop = buttonYtop - height;
                         int allowedYbottom = buttonYbottom;
 
-                        if(((allowedYtop - randomY) >= 0)) System.out.println("ytop");
-                        if(((allowedXleft - randomX) >= 0)) System.out.println("xleft");
-                        if((allowedYbottom <= randomY)) System.out.println("ybot");
-                        if((allowedXright <= randomX)) System.out.println("xright");
-                        System.out.println();
-
-
+                        do {
+                            randomX = ThreadLocalRandom.current().nextInt(0, maxWidth);
+                            randomY = ThreadLocalRandom.current().nextInt(0, maxHeight);
+                        } while (!((allowedYtop - randomY) >= 0) && !((allowedXleft - randomX) >= 0) && !(allowedYbottom <= randomY) && !(allowedXright <= randomX));
 
                         this.x = randomX;
                         this.y = randomY;
 
-                        int random = 5*1000;//ThreadLocalRandom.current().nextInt(0, 10*60*1000);
+                        int random = 5 * 1000;//ThreadLocalRandom.current().nextInt(0, 10*60*1000);
                         Thread.sleep(random);
                         this.setBounds(x, y, width, height);
                         //this.setBackground(Color.red);
@@ -78,7 +71,6 @@ public class Villain extends JButton {
                         this.show();
                     }
                     Thread.sleep(1000);
-                    //System.out.println(isSpawned);
 
                 }
 
@@ -86,13 +78,6 @@ public class Villain extends JButton {
                 e.printStackTrace();
             }
 
-
-
         }).start();
-
-
     }
-
-
-
 }
